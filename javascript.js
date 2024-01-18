8461bc96716d9dba94fbc784219ca88adfc80dbf
Diego Mendoza <diegomendozag1999mg@gmail.com>
2024-01-18 17:43:01 -0500

    Commit5

---
+++ javascript.js
@@ -0,0 +1,11 @@
+const entry = document.querySelector(".the-get-in-button");
+const uDetails = document.querySelector("#username");
+const pDetails = document.querySelector("#password");
+
+entry = addEventListener('click', securityPass);
+
+function securityPass(){
+ const hidden = entry.classList.contains('inactive'); 
+
+}
+
